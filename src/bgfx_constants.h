8d32a6ebe2f66dc2702c28bd0eaadedc14944445
Mitchell Kember <mk12360@gmail.com>
2021-02-21 22:19:53 -0500

    Add constants

--- src/bgfx_constants.h
+++ src/bgfx_constants.h
@@ -3,5 +3,7 @@
 
 // This file is a workaround for https://github.com/ziglang/zig/issues/7971.
 
+#define BGFX_RESET_NONE 0x00000000U
 #define BGFX_RESET_VSYNC 0x00000080U
 #define BGFX_DEBUG_TEXT 0x00000008U
+#define BGFX_DEBUG_STATS 0x00000004U
